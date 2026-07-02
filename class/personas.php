def51a956061c47b1a3e0b92b287b81ef61d4ab1
Brayner Velandia <brayner2007gg@gmail.com>
2026-07-02 15:30:42 -0500

    metodo constructor

--- class/personas.php
+++ class/personas.php
@@ -5,6 +5,16 @@ class persona
     public $edad;
     public $correo;
     public $apellido;
+
+    public function __construct($nombre,$edad,$correo,$apellido)
+    {
+    $this ->nombre = $nombre;
+    $this ->edad = $edad;
+    $this ->correo = $correo;
+    $this ->apellido = $apellido;
+
+    }
+
     public function saludar() {
         return "Mi nombre es : " . $this->nombre . " " . $this->apellido . " y tengo " . $this->edad . " años." . " Mi correo es: " . $this->correo . "<br>";
     }
--- public/index.php
+++ public/index.php
@@ -1,21 +1,14 @@
 <?php
 require_once '../class/personas.php';
 
-$persona1 = new persona();
-$persona1->nombre = "Juan";
-$persona1->apellido = "Perez";
-$persona1->edad = 30;
-$persona1->correo = "juan.perez@example.com";
-
-
-$persona2 = new persona();
-$persona2->nombre = "Maria";
-$persona2->apellido = "Gonzalez";
-$persona2->edad = 25;
-$persona2->correo = "maria.gonzalez@example.com"; 
+$persona1 = new persona("Juan", "30", "juan.perez@example.com", "Perez");
+$persona2 = new persona("Maria", "25", "maria.gonzalez@example.com", "Gonzalez");
 
 echo $persona1->saludar();
 echo $persona2->saludar();
+
+
+
 
 
 
