acc883fdc0f6dddd4a6c63563f10bb02c8ff4522
Brayner Velandia <brayner2007gg@gmail.com>
2026-07-02 15:08:14 -0500

    palabra reservada this

--- class/personas.php
+++ class/personas.php
@@ -4,8 +4,9 @@ class persona
     public $nombre;
     public $edad;
     public $correo;
+    public $apellido;
     public function saludar() {
-        echo "Mi nombre es :";
+        return "Mi nombre es : " . $this->nombre . " " . $this->apellido . " y tengo " . $this->edad . " años." . " Mi correo es: " . $this->correo . "<br>";
     }
     
 } 
--- public/index.php
+++ public/index.php
@@ -1,13 +1,21 @@
 <?php
-require_once 'class/personas.php';
+require_once '../class/personas.php';
 
 $persona1 = new persona();
-
 $persona1->nombre = "Juan";
+$persona1->apellido = "Perez";
+$persona1->edad = 30;
+$persona1->correo = "juan.perez@example.com";
+
+
+$persona2 = new persona();
+$persona2->nombre = "Maria";
+$persona2->apellido = "Gonzalez";
+$persona2->edad = 25;
+$persona2->correo = "maria.gonzalez@example.com"; 
 
 echo $persona1->saludar();
+echo $persona2->saludar();
 
-$edad1 = new edad();
-$edad1->edad = 25;
-echo $edad1->getEdad();
+
 
